2ac0fc5cedf322309507010e1834177261910900
mict <mict@zhaw.ch>
2025-01-24 12:36:26 +0100

    Update logging path

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ class Wav2VecPipeline:
             os.makedirs(self.base_model_path, exist_ok=True)
 
         # Setup logging
-        self.logging_path = str(os.path.join(self.config.alt_base_path, 'logs', "runs", config.group, config.job_type, config.run_id or ""))
+        self.logging_path = str(os.path.join(self.config.alt_base_path, 'logs', "runs", config.group, config.job_type, config.experiment_label or ""))
         if not os.path.exists(self.logging_path):
             os.makedirs(self.logging_path, exist_ok=True)
         self.logger = TrainLogger(self.logging_path)
